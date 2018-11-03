afc77ba881b171a6ac930eb02bdd1cb6aa8ab3b4
tuananh <tuananh@gmail.com>
2018-11-03 22:40:50 +0700

    update

--- app/src/main/java/android/app/hotel/view/contact/ContactActivity.java
+++ app/src/main/java/android/app/hotel/view/contact/ContactActivity.java
@@ -12,7 +12,7 @@ public class ContactActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     TextView mLienHe = null;
-    String LienHe = "Với mong muốn đem đến những giây phút nghỉ ngơi tuyệt vời nhất cho quý khách hàng, AVA Hotel & Apartment sẽ không ngừng hoàn thiện và nâng cao chất lượng dịch vụ. Hãy liên hệ với chúng tôi khi có bất kì thắc mắc, phản hồi hoặc có nhu cầu muốn đặt phòng.\n" +
+    String LienHe = "Với mong muốn đem đến những giây phút nghỉ ngơi tuyệt vời nhất cho quý khách hàng, Dubai Hotel sẽ không ngừng hoàn thiện và nâng cao chất lượng dịch vụ. Hãy liên hệ với chúng tôi khi có bất kì thắc mắc, phản hồi hoặc có nhu cầu muốn đặt phòng.\n" +
             "\n" +
             "Chúng tôi rất hân hạnh được phục vụ quý khách!\n" +
             "Địa chỉ: 39 Trần Bạch Đằng,  An Thượng 1, Phường Mỹ An, Quận Ngũ Hành Sơn, Tp Đà Nẵng\n" +
@@ -23,15 +23,15 @@ public class ContactActivity extends AppCompatActivity {
             "Hotline 2: 0935.291.898\n" +
             "Hotline 3: 0942.688.569\n" +
             "\n" +
-            "Email: avahoteldn@gmail.com\n" +
+            "Email: dubaihoteldn@gmail.com\n" +
             "\n" +
-            "Facebook: www.facebook.com/avahoteldn";
+            "Facebook: www.facebook.com/dubaihoteldn";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle("Hotel Manager");
+        toolbar.setTitle("Liên hệ");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
